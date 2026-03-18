f3ec557396ebcb52624265a4de50d1d39b23d8e6
Soumya Ranjan <ranjansoumya115@gmail.com>
2026-03-18 17:48:25 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "AI DevOps Platform Running 🚀"
+
+@app.route("/load")
+def load():
+    if random.randint(1,10) > 7:
+        return "ERROR: CPU spike detected"
+    return "System Normal"
+
+app.run(host="0.0.0.0", port=5000)
